81887c964d8d190c1eeb565cc860831e37b98e54
3lement3 <djelement@yandex.ru>
2018-02-08 17:16:45 +0300

    Add files via upload

--- ap.js
+++ ap.js
@@ -1,19 +1,29 @@
 let button = document.querySelector('#button');
 let buttonReset = document.querySelector('#reset');
-let name = document.querySelector('#name');
 let url = 'https://swapi.co/api/people/';
+let name = document.querySelector('#name');
+let mass = document.querySelector('#mass');
+
+
 function getInfo() {
     let random = Math.floor((Math.random() * 88) + 1);
     let url = 'https://swapi.co/api/people/' + random;
+
         axios.get(url).then(function(response) {
         updateInfo(response.data)
     })
 }
+
 function getReset() {
     $('#name').empty();
+    $('#mass').empty();
 }
+
+
 function updateInfo(data) {
-    name.innerText = 'Characters:' + ' ' + data.name
+    name.innerText = 'Characters:' + ' ' + data.name;
+    mass.innerText = 'Mass:' + ' ' + data.mass
 }
+
 button.addEventListener('click', getInfo);
 buttonReset.addEventListener('click', getReset);
