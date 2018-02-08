cb2f0204d477d326811ba8bb59e446969ff8393b
3lement3 <djelement@yandex.ru>
2018-02-08 14:15:51 +0300

    Update ap.js

--- ap.js
+++ ap.js
@@ -2,24 +2,18 @@ let button = document.querySelector('#button');
 let buttonReset = document.querySelector('#reset');
 let name = document.querySelector('#name');
 let url = 'https://swapi.co/api/people/';
-
 function getInfo() {
-    let random = Math.floor((Math.random() * 88) + 1)
-    let url = 'https://swapi.co/api/people/' + random
-
+    let random = Math.floor((Math.random() * 88) + 1);
+    let url = 'https://swapi.co/api/people/' + random;
         axios.get(url).then(function(response) {
         updateInfo(response.data)
     })
 }
-
 function getReset() {
     $('#name').empty();
 }
-
-
 function updateInfo(data) {
     name.innerText = 'Characters:' + ' ' + data.name
 }
-
-button.addEventListener('click', getInfo)
-buttonReset.addEventListener('click', getReset)
+button.addEventListener('click', getInfo);
+buttonReset.addEventListener('click', getReset);
